b3d66f2b4a8d9a627b3acd650e8ed2c7e88bbc6f
Kemalele <kemalele17@gmail.com>
2019-10-25 21:30:07 +0600

    Added Entity Builder

---
+++ Shop.Services/EntityBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop.DataAccess;
+
+namespace Shop.Domain
+{
+    public static class EntityBuilder
+    {
+        public static User CreateUser(string phoneNumber, string email,string address,string password)
+        {
+            return new User
+            {
+                PhoneNumber = phoneNumber,
+                Email = email,
+                Address = address,
+                Password = password
+            };
+        }
+
+        public static DbController CreateDbController()
+        {
+            return new DbController();
+        }
+    }
+}
