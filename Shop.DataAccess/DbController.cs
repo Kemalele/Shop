722b2ec699cdffdcbaf2ec9cf54c01503fcca8cf
Kemalele <kemalele17@gmail.com>
2019-10-25 21:27:16 +0600

    Added DbController class

---
+++ Shop.DataAccess/DbController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop.Domain;
+
+namespace Shop.DataAccess
+{
+    public class DbController
+    {
+        public void Insert(Entity enttity)
+        {
+            using(var context = new ShopContext())
+            {
+                context.Add(enttity);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
--- Shop.DataAccess/ShopContext.cs
+++ Shop.DataAccess/ShopContext.cs
@@ -3,25 +3,28 @@ using Shop.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace Shop.DataAccess
 {
     public class ShopContext:DbContext
     {
         private readonly string connectionString;
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
-        public ShopContext(string connectionString)
+        public ShopContext()
         {
-            this.connectionString = connectionString;
+            connectionString = "Server=A-305-01;Database=ShopDb;Trusted_Connection=True;";
             Database.EnsureCreated();
         }
 
-        public DbSet<Category> Categories { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-          //  optionsBuilder.UseSqlServer(connectionString);
-           optionsBuilder.UseInMemoryDatabase("ShopDb");
+            optionsBuilder.UseSqlServer(connectionString);
+           //optionsBuilder.UseInMemoryDatabase("ShopDb");
         }
     }
 }
